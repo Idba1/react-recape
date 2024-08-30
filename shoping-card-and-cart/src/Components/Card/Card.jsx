21ebefc44f417e19e5012fe05bea6e86e42f9504
Idba1 <moniraislamself@gmail.com>
2024-08-30 23:31:35 +0600

    data display using map

--- shoping-card-and-cart/src/Components/Card/Card.jsx
+++ shoping-card-and-cart/src/Components/Card/Card.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 const Card = () => {
 
-    const [Products, setProducts]=useState([])
+    const [Products, setProducts] = useState([])
     // useEffect(()=>{
     //     fetch("./fackData.json")
     //     .then(res => res.json())
@@ -11,34 +11,39 @@ const Card = () => {
     //     })
     // },[])
 
-    useEffect(()=>{
+    useEffect(() => {
         fetch("./fackData.json")
-        .then(res=>res.json())
-        .then(data=>{
-            setProducts(data);
-        })
-    },[])
+            .then(res => res.json())
+            .then(data => {
+                setProducts(data);
+            })
+    }, [])
     console.log(Products);
     return (
         <div>
-            <div className="card bg-base-100 w-96 shadow-xl">
-                <figure>
-                    <img className="w-28"
-                        src="https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg"
-                        alt="Men's T-shirt" />
-                </figure>
-                <div className="card-body">
-                    <h2 className="card-title">
-                        Men's T-shirt!
-                        <div className="badge badge-secondary">NEW</div>
-                    </h2>
-                    <p>If a dog chews Men's T-shirt whose Men's T-shirt does he choose?</p>
-                    <div className="card-actions justify-around mt-5">
-                        <div className="text-2xl">520$</div>
-                        <div className="btn btn-accent text-white badge-outline">Products</div>
-                    </div>
-                </div>
-            </div>
+            {
+                Products.map((product => (
+                    <div key={product.id} className="card bg-base-100 w-96 shadow-xl">
+                        <figure>
+                            <img className="w-28"
+                                src={product.image} />
+                        </figure>
+                        <div className="card-body">
+                            <h2 className="card-title">
+                                {product.title}
+                            </h2>
+                            <p>
+                                {product.category}
+                            </p>
+                            <p>{product.description}</p>
+                            <div className="card-actions justify-around mt-5">
+                                <div className="text-2xl">{product.price}</div>
+                                <div className="btn btn-accent text-white badge-outline">Products</div>
+                            </div>
+                        </div>
+                    </div> 
+                )))
+            }
         </div>
     );
 };
