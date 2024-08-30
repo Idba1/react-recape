c0c059e34df85e5c881c0346d8b437a5e369320f
Idba1 <moniraislamself@gmail.com>
2024-08-31 02:52:47 +0600

    react router dom setup

--- firebase-authentication/src/main.jsx
+++ firebase-authentication/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import {
+  RouterProvider,
+} from "react-router-dom";
+import Router from './Routes/Router/Router';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={Router} />
   </StrictMode>,
 )
