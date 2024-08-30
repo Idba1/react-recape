0d9df548233551fb0b8758befc22889a61d0f267
Idba1 <moniraislamself@gmail.com>
2024-08-30 23:42:03 +0600

    add cart option

--- shoping-card-and-cart/src/App.jsx
+++ shoping-card-and-cart/src/App.jsx
@@ -5,7 +5,18 @@ function App() {
 
   return (
     <>
-      <Card></Card>
+      <div className='flex justify-around'>
+        <div>
+          <Card></Card>
+        </div>
+        <div className='w-1/2'>
+          <h1 className='text-2xl lg:text-4xl'>This is cart</h1>
+          <div className='flex justify-around text-2xl mt-5'>
+            <h5>Name</h5>
+            <h5>Price</h5>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
