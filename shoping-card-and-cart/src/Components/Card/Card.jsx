24a52e449dfc61323eddea04186b76ba3e67bd78
Idba1 <moniraislamself@gmail.com>
2024-08-30 23:25:06 +0600

    fatech data using fatch

--- shoping-card-and-cart/src/Components/Card/Card.jsx
+++ shoping-card-and-cart/src/Components/Card/Card.jsx
@@ -1,10 +1,29 @@
+import { useEffect, useState } from "react";
 
 const Card = () => {
+
+    const [Products, setProducts]=useState([])
+    // useEffect(()=>{
+    //     fetch("./fackData.json")
+    //     .then(res => res.json())
+    //     .then(data=>{
+    //         console.log(data);
+    //     })
+    // },[])
+
+    useEffect(()=>{
+        fetch("./fackData.json")
+        .then(res=>res.json())
+        .then(data=>{
+            setProducts(data);
+        })
+    },[])
+    console.log(Products);
     return (
         <div>
             <div className="card bg-base-100 w-96 shadow-xl">
                 <figure>
-                    <img
+                    <img className="w-28"
                         src="https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg"
                         alt="Men's T-shirt" />
                 </figure>
@@ -14,8 +33,8 @@ const Card = () => {
                         <div className="badge badge-secondary">NEW</div>
                     </h2>
                     <p>If a dog chews Men's T-shirt whose Men's T-shirt does he choose?</p>
-                    <div className="card-actions justify-end">
-                        <div className="btn btn-accent text-white badge-outline">Fashion</div>
+                    <div className="card-actions justify-around mt-5">
+                        <div className="text-2xl">520$</div>
                         <div className="btn btn-accent text-white badge-outline">Products</div>
                     </div>
                 </div>
