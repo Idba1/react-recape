1d0d629a7bc13fa16ede80e3fa55f3a93b0ff928
Idba1 <moniraislamself@gmail.com>
2024-08-30 23:54:40 +0600

    add handlecart

--- shoping-card-and-cart/src/Components/Card/Card.jsx
+++ shoping-card-and-cart/src/Components/Card/Card.jsx
@@ -19,33 +19,39 @@ const Card = () => {
             })
     }, [])
     console.log(Products);
-    return (
-        <div>
-            {
-                Products.map((product => (
-                    <div key={product.id} className="card bg-base-100 w-96 shadow-xl">
-                        <figure>
-                            <img className="w-28"
-                                src={product.image} />
-                        </figure>
-                        <div className="card-body">
-                            <h2 className="card-title">
-                                {product.title}
-                            </h2>
-                            <p>
-                                {product.category}
-                            </p>
-                            <p>{product.description}</p>
-                            <div className="card-actions justify-around mt-5">
-                                <div className="text-2xl">{product.price}</div>
-                                <div className="btn btn-accent text-white badge-outline">Products</div>
-                            </div>
+
+    const handleCart=(p)=> 
+        {
+            console.log("product");
+        }
+    ;
+return (
+    <div>
+        {
+            Products.map((product => (
+                <div key={product.id} className="card bg-base-100 w-96 shadow-xl">
+                    <figure>
+                        <img className="w-28"
+                            src={product.image} />
+                    </figure>
+                    <div className="card-body">
+                        <h2 className="card-title">
+                            {product.title}
+                        </h2>
+                        <p>
+                            {product.category}
+                        </p>
+                        <p>{product.description}</p>
+                        <div className="card-actions justify-around mt-5">
+                            <div className="text-2xl">{product.price}</div>
+                            <div onClick={()=>handleCart()} className="btn btn-accent text-white badge-outline">Products</div>
                         </div>
-                    </div> 
-                )))
-            }
-        </div>
-    );
+                    </div>
+                </div>
+            )))
+        }
+    </div>
+);
 };
 
 export default Card;
