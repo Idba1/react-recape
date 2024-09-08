cd096564d6b42c62efd447790513a077c13cbcf5
Idba1 <moniraislamself@gmail.com>
2024-09-08 23:11:23 +0600

    connect with mongodb

--- GigaGadgets/B9-Crud-client-server/index.js
+++ GigaGadgets/B9-Crud-client-server/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors')
 require('dotenv').config()
 const port = process.env.PORT || 9000
@@ -7,7 +8,7 @@ const app = express()
 const corsOptions = {
     origin: [
         'http://localhost:5173',
-        'http://localhost:5174', ,
+        'http://localhost:5174', 
     ],
     credentials: true,
     optionSuccessStatus: 200,
@@ -15,6 +16,28 @@ const corsOptions = {
 app.use(cors(corsOptions))
 app.use(express.json())
 
+const uri = `mongodb+srv://GigaGadgets:IAPLj4b3n7iYQqMA@cluster0.wwse58h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Hey GigaGadgets!')
